32f75c239d5028ab33d6817edd5588d3c6919aea
Nguyen Toan <toanmnh2002@gmail.com>
2025-03-24 11:37:45 +0700

    [Exercise 4] Refine comments in SQL script for clarity and consistency

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -4,7 +4,7 @@ SET UnitPrice = UnitPrice * 1.10 -- Sets the UnitPrice to 110% of its current va
 WHERE CategoryID = (
     SELECT CategoryID
     FROM Categories
-    WHERE CategoryName = 'Beverages' -- The subquery retrieves the CategoryID for "Beverages" from the Categories table.
+    WHERE CategoryName = 'Beverages' -- The subquery retrieves the CategoryID for "Beverages" from the 'Categories' table.
 );
 
 -- This query inserts a new product named "Organic Coffee" into the Products table, assigning it to the "Beverages" category, with a specified price and stock quantity, and a supplier chosen randomly.
@@ -19,11 +19,11 @@ INSERT INTO Products
 VALUES 
 (
     'Organic Coffee', -- 'Organic Coffee' as the ProductName.
-    (SELECT SupplierID FROM Suppliers LIMIT 1), -- Randomly selects a SupplierID from the Suppliers table.
+    (SELECT SupplierID FROM Suppliers LIMIT 1), -- Randomly selects a SupplierID from the 'Suppliers' table.
     (
     SELECT CategoryID 
     FROM Categories 
-    WHERE CategoryName = 'Beverages' -- The CategoryID for "Beverages" retrieved from the Categories table.
+    WHERE CategoryName = 'Beverages' -- The CategoryID for "Beverages" retrieved from the 'Categories' table.
     ),
     25.00, -- Unit price of the product.    
     100 -- Units in stock for the product.
