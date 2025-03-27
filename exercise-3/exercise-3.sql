fe53bd390ced0db8ee9e32d80757bbd53002e3f6
Nguyen Toan <toanmnh2002@gmail.com>
2025-03-27 08:56:04 +0700

    [Exercise 3] Fix SQL syntax by removing quotes around alias for SupplierName

--- exercise-3/exercise-3.sql
+++ exercise-3/exercise-3.sql
@@ -1,5 +1,5 @@
 -- This query retrieves the list of products along with their category names and supplier names.
-SELECT product.ProductName, category.CategoryName, supplier.CompanyName As 'SupplierName'
+SELECT product.ProductName, category.CategoryName, supplier.CompanyName As SupplierName
 FROM Products product
 JOIN Categories category ON product.CategoryID = category.CategoryID -- Joining the Products table with the 'Categories' table on the CategoryID column.
 JOIN Suppliers supplier ON product.SupplierID = supplier.SupplierID; -- Joining the Products table with the 'Suppliers' table on the SupplierID column.
